3edd4d1a301a49b73031faa66f9aa366e2e53f14
jpic <jpic@yourlabs.org>
2019-10-20 12:44:47 +0200

    OpenShift 3 Template

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-AUTH_USER_MODEL = 'ferme.User'
+AUTH_USER_MODEL = 'user.User'
 
 
 # Quick-start development settings - unsuitable for production
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ferme',
+    'user',
 ]
 
 MIDDLEWARE = [
