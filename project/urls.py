fce16c831c2405cc2257655e5d7f65167a60606e
jpic <jpic@yourlabs.org>
2019-10-20 12:14:02 +0200

    Add health view

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
+from django import http
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
+    path('health', lambda req: http.HttpResponse(200)),
     path('admin/', admin.site.urls),
 ]
